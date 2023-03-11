d6e5ca6699eb4747f554336dfeaa3d3cb7469857
Nidhi <nidhiparabmail@gmail.com>
2023-03-11 18:19:41 +0530

    signup design

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
@@ -12,9 +12,9 @@
   </head>
 <body>
 <?php require 'partials/_nav.php' ?>
-    <h1>Sign Up Form!</h1>
-    <div class="container">
-        <form action="/LOC 5.0/signup.php" method="post">
+<div class="container">
+    <form action="/LOC 5.0/signup.php" method="post">
+            <h1>Sign Up Form!</h1>
         <div class="form-row">
                 <div class="col-md-4 mb-3">
                 <label for="validationServer01">First name</label>
